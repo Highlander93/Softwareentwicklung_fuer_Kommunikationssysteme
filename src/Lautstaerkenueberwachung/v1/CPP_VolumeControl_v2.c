32e955e7c4bd8c2fad48935e56b9abfc63eddddd
Dr. Müller <mueller-steven@hotmail.de>
2023-06-06 11:43:11 +0200

    Update CPP_VolumeControl_v2.c

--- src/Lautstaerkenueberwachung/v1/CPP_VolumeControl_v2.c
+++ src/Lautstaerkenueberwachung/v1/CPP_VolumeControl_v2.c
@@ -1,125 +1,149 @@
-
-enum PinId { POTENTIOMETER_PIN = A0, GREEN_LED_PIN = 4, YELLOW_LED_PIN = 5, RED_LED_PIN = 6 };
-
 class LED {
 private:
-    PinId pin;
+    int pin;
 
 public:
-    LED(PinId pinNumber) : pin(pinNumber) {
+    LED(int pin) : pin(pin) {
         pinMode(pin, OUTPUT);
+        digitalWrite(pin, LOW);
     }
 
-    void on() {
+    void turnOn() {
         digitalWrite(pin, HIGH);
     }
 
-    void off() {
+    void turnOff() {
         digitalWrite(pin, LOW);
     }
 };
 
+class Potentiometer {
+private:
+    int pin;
+    int currentValue;
+    int lastValue;
+
+public:
+    Potentiometer(int pin) : pin(pin), currentValue(0), lastValue(-1) {
+        pinMode(pin, INPUT);
+    }
+
+    int readValue() {
+        currentValue = analogRead(pin);
+        return currentValue;
+    }
+
+    bool hasChanged() {
+        if (currentValue != lastValue) {
+            lastValue = currentValue;
+            return true;
+        }
+        return false;
+    }
+};
+
+enum class VolumeLevel { UNDEFINED, OK, STILL_OK, TOO_LOUD };
+
+const int MIN_VOLUME = 30; //in dB
+const int MAX_VOLUME = 110; //in dB
+const int MAX_ANALOG_IN = 1023;
+const int PLEASANT_THRESHOLD = 60; //in dB
+const int HEARING_DAMAGE_THRESHOLD = 90; // in dB
+
 class VolumeControl {
 private:
-    int currentPotentiometer;
-    int lastPotentiometer;
-    const int MIN_VOLUME;
-    const int MAX_VOLUME;
-    const int MAX_ANALOG_IN;
-    double decibel;
-    int loudness;
-
-    const int PLEASANT_THRESHOLD;
-    const int HEARING_DAMAGE_THRESHOLD;
-
-    enum VolumeLevel { UNDEFINED, OK, STILL_OK, TOO_LOUD };
-    VolumeLevel lastVolumeLevel;
-    VolumeLevel currentVolumeLevel;
-
+    Potentiometer potentiometer;
     LED greenLED;
     LED yellowLED;
     LED redLED;
+    VolumeLevel lastVolumeLevel;
+    VolumeLevel currentVolumeLevel;
 
 public:
-    VolumeControl()
-        : currentPotentiometer(0), lastPotentiometer(-1), MIN_VOLUME(30), MAX_VOLUME(110), MAX_ANALOG_IN(1023),
-          PLEASANT_THRESHOLD(60), HEARING_DAMAGE_THRESHOLD(90), lastVolumeLevel(UNDEFINED),
-          currentVolumeLevel(UNDEFINED), greenLED(GREEN_LED_PIN), yellowLED(YELLOW_LED_PIN), redLED(RED_LED_PIN) {}
+    VolumeControl(int potPin, int greenPin, int yellowPin, int redPin)
+        : potentiometer(potPin),
+          greenLED(greenPin),
+          yellowLED(yellowPin),
+          redLED(redPin),
+          lastVolumeLevel(VolumeLevel::UNDEFINED),
+          currentVolumeLevel(VolumeLevel::UNDEFINED) {}
 
-    void setup()
-    {
-        pinMode(POTENTIOMETER_PIN, INPUT);
-
-        digitalWrite(GREEN_LED_PIN, LOW);
-        digitalWrite(YELLOW_LED_PIN, LOW);
-        digitalWrite(RED_LED_PIN, LOW);
-
-        greenLED.off();
-        yellowLED.off();
-        redLED.off();
+    void setup() {
+        // Nothing to do here
     }
 
-    void loop()
-    {
-        currentPotentiometer = analogRead(POTENTIOMETER_PIN);
+    void loop() {
+        potentiometer.readValue();
+        if (potentiometer.hasChanged()) {
+            double decibel = MIN_VOLUME + double(MAX_VOLUME - MIN_VOLUME) * potentiometer.readValue() / MAX_ANALOG_IN;
+            int loudness = static_cast<int>(decibel + 0.5);
 
-        if (currentPotentiometer != lastPotentiometer)
-        {
-            decibel = MIN_VOLUME + double(MAX_VOLUME - MIN_VOLUME) * currentPotentiometer / MAX_ANALOG_IN;
-            loudness = int(decibel + 0.5);
-
-            currentVolumeLevel = TOO_LOUD;
-            if (loudness < PLEASANT_THRESHOLD)
-            {
-                currentVolumeLevel = OK;
-            }
-            else if (loudness < HEARING_DAMAGE_THRESHOLD)
-            {
-                currentVolumeLevel = STILL_OK;
+            if (loudness < PLEASANT_THRESHOLD) {
+                currentVolumeLevel = VolumeLevel::OK;
+            } else if (loudness < HEARING_DAMAGE_THRESHOLD) {
+                currentVolumeLevel = VolumeLevel::STILL_OK;
+            } else {
+                currentVolumeLevel = VolumeLevel::TOO_LOUD;
             }
 
-            if (currentVolumeLevel != lastVolumeLevel)
-            {
-                if (lastVolumeLevel != UNDEFINED)
-                {
-                    LED led = getLEDFromVolumeLevel(lastVolumeLevel);
-                    led.off();
+            if (currentVolumeLevel != lastVolumeLevel) {
+                if (lastVolumeLevel != VolumeLevel::UNDEFINED) {
+                    turnOffLED(lastVolumeLevel);
                 }
-
-                LED led = getLEDFromVolumeLevel(currentVolumeLevel);
-                led.on();
-
+                turnOnLED(currentVolumeLevel);
                 lastVolumeLevel = currentVolumeLevel;
             }
-
-            lastPotentiometer = currentPotentiometer;
         }
 
         delay(100);
     }
 
-    LED getLEDFromVolumeLevel(VolumeLevel level)
-    {
-        switch (level)
-        {
-            case OK:
-                return greenLED;
-            case STILL_OK:
-                return yellowLED;
+private:
+    void turnOnLED(VolumeLevel level) {
+        switch (level) {
+            case VolumeLevel::OK:
+                greenLED.turnOn();
+                break;
+            case VolumeLevel::STILL_OK:
+                yellowLED.turnOn();
+                break;
+            case VolumeLevel::TOO_LOUD:
+                redLED.turnOn();
+                break;
             default:
-                return redLED;
+                break;
+        }
+    }
+
+    void turnOffLED(VolumeLevel level) {
+        switch (level) {
+            case VolumeLevel::OK:
+                greenLED.turnOff();
+                break;
+            case VolumeLevel::STILL_OK:
+                yellowLED.turnOff();
+                break;
+            case VolumeLevel::TOO_LOUD:
+                redLED.turnOff();
+                break;
+            default:
+                break;
         }
     }
 };
 
-VolumeControl volumeControl;
+// Pin definitions
+const int POTENTIOMETER_PIN = A0;
+const int GREEN_LED_PIN = 4;
+const int YELLOW_LED_PIN = 5;
+const int RED_LED_PIN = 6;
 
-void setup()
-{
+VolumeControl volumeControl(POTENTIOMETER_PIN, GREEN_LED_PIN, YELLOW_LED_PIN, RED_LED_PIN);
+
+void setup() {
     volumeControl.setup();
 }
 
-void loop()
-{
+void loop() {
     volumeControl.loop();
 }
